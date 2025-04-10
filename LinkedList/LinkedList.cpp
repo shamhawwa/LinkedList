5765d084ada50305e58856e425d8e87a582c266d
shamhawwa <shamhawwa621@gmail.com>
2025-04-10 15:50:02 +0300

    Testing DLL methods

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 #include "SinglyLinkedList.cpp"
+#include "DoublyLinkedList.cpp"
 using namespace std;
 
 int main()
 {
     SinglyLinkedList sll;
     cout << "-----------------------------------------------\n"<<"Singly Linked List Example: \n";
+
     sll.addToHead(1);
     sll.addToTail(2);
     sll.addToTail(4);
     sll.addToTail(5);
     sll.addToTail(6);
     sll.printSLL();
+
     sll.length();
     sll.contains(3);
 
@@ -29,7 +32,41 @@ int main()
     sll.removeAfterNode(3);
     sll.printSLL();
     sll.length();
-    cout << "-----------------------------------------------\n";
 
+    cout << "-----------------------------------------------\n"<<"Doubly Linked List Example: \n";
+    DoublyLinkedList dll;
+    dll.addToHead(1);
+    dll.addToTail(2);
+    dll.addToTail(3);
+    dll.addToTail(4);
+    dll.addToTail(5);
+    dll.addToTail(6);
+    dll.addToTail(7);
+    dll.addToTail(8);
+    dll.printDLLForward();
+    dll.printDLLBackward();
+
+    dll.addAfterNode(8, 10);
+    dll.printDLLForward();
+
+    dll.contains(9);
+    dll.addBeforeNode(10, 9);
+    dll.printDLLForward();
+
+    dll.contains(9);
+    dll.length();
+
+    dll.removeHead();
+    dll.printDLLForward();
+    dll.removeTail();
+    dll.printDLLForward();
+    dll.removeElement(2);
+    dll.printDLLForward();
+    dll.removeAfterNode(5);
+    dll.printDLLForward();
+    dll.removeBeforeNode(8);
+    dll.printDLLForward();
+    dll.length();
+    cout << "-----------------------------------------------\n";
     return 0;
 }
